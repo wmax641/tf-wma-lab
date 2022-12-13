fc24a7c0bd9c63cefc2c4f643b7a5cc671d9e8d1
wmax641 <contact@wmax641.website>
2022-12-13 15:42:48 +1100

    fix format

--- resources.tf
+++ resources.tf
@@ -1,8 +1,6 @@
 # Create a s3 bucket. 
-# The bucket name will equal to what 'base_name' is set in variables.tf
 resource "aws_s3_bucket" "my_bucket" {
   bucket = var.project_name
-
 }
 
 resource "aws_s3_object" "datafiles" {
